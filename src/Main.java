6168b108191ea365198a319ca79bfff1b87ea2c7
Joshua smith <joshuaadamsmith2003@gmail.com>
2025-11-25 15:19:52 -0800

    day 1 changes

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
 /*
- *
+ * Description:
+ * 1. Given a positive integer n, list all the moves required in the Tower of Hanoi puzzle to move n disks from one peg
+ to another according to the rules of the puzzle.
+ * 2. Given a positive integer n, list all the bit sequences of length n that do not have a pair of consecutive Os.
+ * 3. Given an integer n greater than 1, write out all ways to parenthesize the product of n + 1 variables.
+ * (choosing option 1)
+ * Author: joshua smith <joshuaadasmith2003@gmail.com>
+ * version: 1.0
  */
+
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
+        Scanner keyboard= new Scanner(System.in);
 
     }
+
 }
